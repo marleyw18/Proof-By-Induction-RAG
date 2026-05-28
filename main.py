022fb07e79d102ca36142893aea60513a531defe
Marley <marleyw041803@gmail.com>
2026-05-28 03:01:21 -0400

    Moved some files. Shelved UI for later

---
+++ main.py
@@ -0,0 +1,43 @@
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+
+# System rubric
+with open('prompt', "r") as file:
+    prompt = file.read()
+    print("...System prompt loaded")
+
+system_prompt = ChatPromptTemplate.from_template(prompt + '{user_message}')
+
+# initialize the model
+model = ChatOllama(model="llama3.1")
+
+# create the chain (going back and forth with the model)
+chain = system_prompt | model
+
+# conversation history
+history = {}
+
+print('Hi! I am your proof by induction tutor! Ask away. \n')
+def chat():
+    while True:
+        user_message = input("\nYou: ")
+        if user_message.lower() == 'exit':
+            print('Goodbye')
+            break
+        else:
+            print("\nTutor: ", end="", flush=True)
+            full_response = ""
+
+            # print text as it renders
+            for result in chain.stream({'user_message': user_message}):
+                print(result.content, end="", flush=True)
+
+                # store the full response as it is created
+                full_response += result.content
+
+                # add to conversation history
+                history[user_message] = full_response
+
+
+if __name__ == "__main__":
+    chat()
